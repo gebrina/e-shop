946ecb8b173686a83aac49f43135936de23efd33
gebrina <gsonofmika@gmail.com>
2023-10-01 15:21:57 +0300

    created login page

--- src/pages/user/login/Login.tsx
+++ src/pages/user/login/Login.tsx
@@ -1,26 +1,65 @@
+import { Button } from "primereact/button";
+import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { useFormik } from "formik";
+import { NavLink } from "react-router-dom";
+import { loginValidtion } from "../../../utils/validations";
 
 const Login = () => {
   const { handleSubmit, values, handleChange, errors, touched } = useFormik({
-    initialValues: { email: "", paswword: "" },
+    initialValues: { email: "", password: "" },
+    validationSchema: loginValidtion,
     onSubmit: () => {},
   });
 
   return (
     <main className="container d-flex  justify-content-center align-items-center">
-      <section className="col-md-6 ">
-        <form>
-          <span className="p-float-label">
-            <InputText
-              id="email"
-              value={values.email}
-              name="email"
-              onChange={handleChange}
-            />
-            <label htmlFor="email">Email Address</label>
-          </span>
-        </form>
+      <section className="col-md-5 col-sm-12">
+        <Card title="Login" className="text-center">
+          <form onSubmit={handleSubmit}>
+            <div className="">
+              <div className="p-float-label">
+                <InputText
+                  id="email"
+                  className={`${errors.email && "p-invalid"} w-100`}
+                  value={values.email}
+                  name="email"
+                  onChange={handleChange}
+                />
+                <label htmlFor="email">Email Address</label>
+              </div>
+              {errors.email && touched.email && (
+                <small className="text-danger">{errors.email}</small>
+              )}
+            </div>
+            <div className="my-2">
+              <div className="p-float-label">
+                <InputText
+                  id="password"
+                  className={`${errors.password && "p-invalid"} w-100`}
+                  type="password"
+                  value={values.password}
+                  name="password"
+                  onChange={handleChange}
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+              {errors.password && touched.password && (
+                <small className="text-danger">{errors.password}</small>
+              )}
+            </div>
+            <Button className="btn btn-success w-50 sm:w-100 my-4 mx-auto d-block">
+              Login
+            </Button>
+            <p className="text-center">
+              Don't have account &nbsp;
+              <NavLink className={"text-danger"} to={"/user/register"}>
+                register
+              </NavLink>
+              !
+            </p>
+          </form>
+        </Card>
       </section>
     </main>
   );
