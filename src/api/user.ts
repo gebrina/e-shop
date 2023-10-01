ef369689322220d4254ce4d1ad6c03e58f5890a8
gebrina <gsonofmika@gmail.com>
2023-10-01 18:43:28 +0300

    create user api

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,1 +1,14 @@
+import { IUser } from "../types/user";
 import axios from "./config/axios";
+
+export const findAllUsers = async (): Promise<IUser[]> => {
+  return await axios.get("/users");
+};
+
+export const findOneUser = async (id: string): Promise<IUser> => {
+  return await axios.get(`/users/${id}`);
+};
+
+export const createUser = async (user: IUser): Promise<IUser> => {
+  return await axios.post("/users", user);
+};
