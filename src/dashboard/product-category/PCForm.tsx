d1579a9965e904a43bca4c2131f3fb8fdc18481a
gebrina <gsonofmika@gmail.com>
2023-10-03 16:46:30 +0300

    updated PCForm

--- src/dashboard/product-category/PCForm.tsx
+++ src/dashboard/product-category/PCForm.tsx
@@ -1,36 +1,44 @@
 import { useFormik } from "formik";
+import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { FC } from "react";
 
 export type PCFormProps = {
-  action: "update" | "new";
+  action: "update" | "new" | undefined;
 };
 
-const PCForm: FC<PCFormProps> = () => {
+const PCForm: FC<PCFormProps> = ({ action }) => {
   const { values, handleChange, handleSubmit, errors, touched } = useFormik({
     initialValues: { name: "" },
     onSubmit: () => {},
   });
 
   return (
-    <form className="card" onSubmit={handleSubmit}>
-      <div className="my-4">
-        <div className="p-float-label">
-          <InputText
-            id="password"
-            className={`${errors.name && "p-invalid"} w-100`}
-            type="name"
-            value={values.name}
-            name="name"
-            onChange={handleChange}
-          />
-          <label htmlFor="name">Name</label>
-        </div>
-        {errors.name && touched.name && (
-          <small className="text-center text-danger">{errors.name}</small>
-        )}
-      </div>
-    </form>
+    <section className="center-items">
+      <Card
+        title={action == "new" ? "Create Category" : "Update Category"}
+        className="col-md-5"
+      >
+        <form onSubmit={handleSubmit}>
+          <div>
+            <div className="p-float-label">
+              <InputText
+                id="password"
+                className={`${errors.name && "p-invalid"} w-100`}
+                type="name"
+                value={values.name}
+                name="name"
+                onChange={handleChange}
+              />
+              <label htmlFor="name">Name</label>
+            </div>
+            {errors.name && touched.name && (
+              <small className="text-center text-danger">{errors.name}</small>
+            )}
+          </div>
+        </form>
+      </Card>
+    </section>
   );
 };
 
--- src/dashboard/product-category/ProductCategory.tsx
+++ src/dashboard/product-category/ProductCategory.tsx
@@ -1,9 +1,25 @@
+import { Button } from "primereact/button";
+import { useState } from "react";
+import { FiPlus } from "react-icons/fi";
+import PCForm from "./PCForm";
+
 const ProductCategory = () => {
+  const [action, setAction] = useState();
+
   return (
-    <div>
-      ProductCategory
+    <section className="my-3">
+      <div className="right-items">
+        <Button
+          onClick={() => setAction("new")}
+          className="btn btn-success  center-items"
+        >
+          <FiPlus />
+          <span>Category</span>
+        </Button>
+      </div>
+      {action && <PCForm action={action} />}
       <div className="bg-light"></div>
-    </div>
+    </section>
   );
 };
 
