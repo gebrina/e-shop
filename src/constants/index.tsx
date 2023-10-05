619f3915b099fb892fd699cff7cea0735a69ecda
gebrina <gsonofmika@gmail.com>
2023-10-05 21:56:37 +0300

    created keys for product

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,4 +1,11 @@
+//PRODUCT CATEGORY KEYS
 export const GET_PRODUCT_CATEGORY_KEY = "pro_cat";
 export const CREATE_PRODUCT_CATEGORY_KEY = "create_pro_cat";
 export const UPDATE_PRODUCT_CATEGORY_KEY = "update_pro_cat";
 export const DELETE_PRODUCT_CATEGORY_KEY = "delete_pro_cat";
+
+//PRODUCT KEYS
+export const GET_PRODUCT_KEY = "product";
+export const CREATE_PRODUCT_KEY = "create_product";
+export const DELETE_PRODUCT_KEY = "delete_product";
+export const UPDATE_PRODUCT_KEY = "updated_produt";
