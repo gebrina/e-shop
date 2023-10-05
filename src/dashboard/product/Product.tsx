c068f023289794ba4062d02ad011c11f91ddf058
gebrina <gsonofmika@gmail.com>
2023-10-05 22:11:41 +0300

    created product form and updated

--- src/dashboard/product/Product.tsx
+++ src/dashboard/product/Product.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+import { DashButtons } from "../common";
+import { Action } from "../common/Buttons";
+import ProductForm from "./ProductForm";
+
 const Product = () => {
-  return <div>Product</div>;
+  const [action, setAction] = useState<Action>();
+
+  const handleClick = () => {
+    if (action) {
+      setAction(undefined);
+    } else {
+      setAction("add");
+    }
+  };
+
+  return (
+    <section className="mt-2">
+      <DashButtons
+        action={action}
+        title="Product"
+        text=""
+        onClick={handleClick}
+      />
+      {action && <ProductForm action={action} />}
+    </section>
+  );
 };
 
 export default Product;
--- src/dashboard/product/ProductForm.tsx
+++ src/dashboard/product/ProductForm.tsx
@@ -1,5 +1,30 @@
-const ProductForm = () => {
-  return <section>ProductForm</section>;
+import { FC } from "react";
+import { useFormik } from "formik";
+import { Card } from "primereact/card";
+import { Action } from "../common/Buttons";
+
+type ProductFormProps = {
+  action: Action;
+};
+const ProductForm: FC<ProductFormProps> = ({ action }) => {
+  const initialValues = {
+    name: "",
+    price: 0,
+    description: "",
+    quantity: 0,
+  };
+  const { values, errors, touched, handleChange, handleSubmit, handleReset } =
+    useFormik({
+      initialValues,
+      onSubmit: () => {},
+    });
+  return (
+    <section>
+      <Card title={(action == "add" ? "Add" : "Update") + " Product"}>
+        <form></form>
+      </Card>
+    </section>
+  );
 };
 
 export default ProductForm;
