d21042ccb02138ebc99eaa7bcd840fa24ff721cc
gebrina <gsonofmika@gmail.com>
2023-10-08 15:04:36 +0300

    updated users dashboard page

--- src/dashboard/user/User.tsx
+++ src/dashboard/user/User.tsx
@@ -4,16 +4,21 @@ import { getAllUsers } from "../../api/user";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import ErrorPage from "../../components/error";
+import { AxiosError } from "axios";
+import Loader from "../../components/loader";
 
 const User = () => {
   const { isLoading, error, data } = useQuery({
     queryKey: [GET_USER_KEY],
     queryFn: getAllUsers,
   });
-  if (error) return <ErrorPage />;
+
+  if (isLoading) return <Loader />;
+  if (error) return <ErrorPage error={(error as AxiosError).message} />;
 
   return (
     <section className="my-3 bg-light mx-auto col-md-6">
+      <h1 className="text-info my-2">Users</h1>
       {!isLoading && (
         <DataTable value={data} paginator rows={5}>
           <Column field="username" header="Name" />
