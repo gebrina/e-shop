059fd612e67aa244a05c9b58402a2cc4be9ad6c8
gebrina <gsonofmika@gmail.com>
2023-10-08 14:33:44 +0300

    added update order functionality

--- src/dashboard/order/Order.tsx
+++ src/dashboard/order/Order.tsx
@@ -1,9 +1,9 @@
 import { Column } from "primereact/column";
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { FiTrash } from "react-icons/fi";
-import { GET_ORDER_KEY } from "../../constants";
-import { getAllOrders } from "../../api/order";
+import { GET_ORDER_KEY, UPDATE_ORDER_KEY } from "../../constants";
+import { getAllOrders, updateOrder } from "../../api/order";
 import ErrorPage from "../../components/error";
 import Loader from "../../components/loader";
 import { DataTable } from "primereact/datatable";
@@ -12,6 +12,7 @@ import { Dropdown } from "primereact/dropdown";
 import { OrderStatus, statusOptions } from "./OrderStatus";
 import { getFormatedDate } from "../../utils";
 import { AxiosError } from "axios";
+import Notification, { NotificationType } from "../common/Notification";
 
 const Order = () => {
   const { isLoading, error, data } = useQuery({
@@ -19,6 +20,14 @@ const Order = () => {
     queryFn: getAllOrders,
   });
 
+  const client = new QueryClient();
+
+  const { mutate: handleUpdate } = useMutation({
+    mutationKey: [UPDATE_ORDER_KEY],
+    mutationFn: updateOrder,
+  });
+
+  const [type, setType] = useState<NotificationType>();
   const [orderStatus, setOrderStatus] = useState<OrderStatus>();
 
   if (isLoading) return <Loader />;
@@ -30,15 +39,42 @@ const Order = () => {
     <FiTrash onClick={handleDelete} className="action-button text-danger" />
   );
 
-  const handleChangeOrderStatus = (id: string, status: number) => {
+  const handleSuccess = () => {
+    setType("success");
+    client.invalidateQueries([GET_ORDER_KEY]);
+  };
+
+  const handleError = () => {
+    setType("error");
+  };
+
+  const handleChangeOrderStatus = (
+    id: string,
+    status: number,
+    order?: IOrder
+  ) => {
+    setType(undefined);
     setOrderStatus({ id, status });
+
+    handleUpdate(
+      {
+        ...order,
+        status,
+      },
+      {
+        onSuccess: handleSuccess,
+        onError: handleError,
+      }
+    );
   };
 
   const orderStatusBody = (data: IOrder) => {
     return (
       <Dropdown
         value={orderStatus?.id == data.id ? orderStatus?.status : data.status}
-        onChange={(e) => handleChangeOrderStatus(data?.id as string, e.value)}
+        onChange={(e) =>
+          handleChangeOrderStatus(data?.id as string, e.value, data)
+        }
         className="px-0 center-items"
         optionLabel="label"
         options={statusOptions}
@@ -49,8 +85,10 @@ const Order = () => {
   const dateColumnBody = (date: string | undefined) => (
     <span>{getFormatedDate(date as string)}</span>
   );
+
   return (
     <section className="my-5">
+      <Notification type={type} title="Order" />
       <h1 className="text-info">Orders</h1>
       <DataTable paginator rows={5} className="col-md-10 mx-auto" value={data}>
         <Column
