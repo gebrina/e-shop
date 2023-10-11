d81ccb04e6710cc9086a783d1d416b6f8587c9a8
gebrina <gsonofmika@gmail.com>
2023-10-11 14:54:01 +0300

    added reset product functionality

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -10,6 +10,8 @@ import { Dropdown } from "primereact/dropdown";
 import { useEffect, useState } from "react";
 import { IProduct } from "../../types/product";
 import ProductCard from "./ProductCard";
+import { Button } from "primereact/button";
+import { FiX } from "react-icons/fi";
 
 export const Product = () => {
   const { data: productCategories } = useQuery({
@@ -55,18 +57,32 @@ export const Product = () => {
     </div>
   );
 
-  console.log(products);
+  const handleResetProducts = () => {
+    setCategoryId("");
+    setCategorisedProducts(products);
+  };
+
   return (
     <section className="container">
-      <Dropdown
-        value={categoryId}
-        onChange={(e) => handleCategoryChange(e.value)}
-        placeholder="Select Category"
-        options={productCategories}
-        optionLabel="name"
-        optionValue="id"
-      />
-
+      <div className="row ">
+        <Dropdown
+          className="col-md-4"
+          value={categoryId}
+          onChange={(e) => handleCategoryChange(e.value)}
+          placeholder="Select Category"
+          options={productCategories}
+          optionLabel="name"
+          optionValue="id"
+        />
+        {categoryId && (
+          <Button
+            onClick={handleResetProducts}
+            className="col-md-1 btn left-items action-button fs-3"
+          >
+            <FiX />
+          </Button>
+        )}
+      </div>
       <section className="products row">
         {categorizedProducts?.map((product) => productCard(product))}
       </section>
--- src/pages/product/ProductCard.tsx
+++ src/pages/product/ProductCard.tsx
@@ -1,13 +1,27 @@
 import { FC } from "react";
 import { IProduct } from "../../types/product";
 import { Card } from "primereact/card";
+import { Button } from "primereact/button";
 
 type ProductCardProps = {
   product: IProduct;
 };
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
-  return <Card title={product.name}></Card>;
+  const { name, price, quantity, id, category, image } = product;
+
+  const header = () => (
+    <img src={import.meta.env.VITE_APP_API_URL + image} alt={name} />
+  );
+
+  const footer = () => (
+    <div className="footer">
+      <span>Price: {price}</span>
+      <span>Quantity: {quantity}</span>
+      <Button className="btn btn-light">Add to Cart</Button>
+    </div>
+  );
+  return <Card header={header} footer={footer} title={name}></Card>;
 };
 
 export default ProductCard;
