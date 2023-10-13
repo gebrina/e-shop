8f357f830d96fd64527b8db17f6c4f6e5c771c35
gebrina <gsonofmika@gmail.com>
2023-10-13 12:17:15 +0300

    added price and quanity

--- src/pages/product/details/ProductDetails.tsx
+++ src/pages/product/details/ProductDetails.tsx
@@ -1,20 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { useParams } from "react-router-dom";
-
-import "./ProductDetais.scss";
+import { AxiosError } from "axios";
 import { GET_SINGLE_PRODUCT_KEY } from "../../../constants";
 import { getSingleProduct } from "../../../api/product";
 import Loader from "../../../components/loader";
 import ErrorPage from "../../../components/error";
-import { AxiosError } from "axios";
+import "./ProductDetais.scss";
+import { Card } from "primereact/card";
 
 export const ProductDetails = () => {
   const { id } = useParams();
 
-  const { data, isLoading, error } = useQuery({
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: [GET_SINGLE_PRODUCT_KEY],
-    queryFn: () => getSingleProduct(id),
+    queryFn: () => getSingleProduct(id ?? ""),
     enabled: !!id,
   });
 
@@ -22,5 +26,34 @@ export const ProductDetails = () => {
 
   if (error) return <ErrorPage error={error as AxiosError} />;
 
-  return <section></section>;
+  const cardFooter = () => (
+    <div>
+      <p>Price: ${product?.price}</p>
+      <p>Quantity: {product?.quantity}</p>
+    </div>
+  );
+
+  return (
+    <section className="container my-5 py-4">
+      <div className="row">
+        <Card
+          className="col-md-6 text-center pro-card"
+          title={product?.name}
+          subTitle={product?.category?.name}
+        >
+          <img
+            className="pro-img"
+            src={import.meta.env.VITE_APP_API_URL + product?.image}
+            alt={product?.name}
+          />
+        </Card>
+
+        <div className="col-md-6">
+          <div
+            dangerouslySetInnerHTML={{ __html: product?.description ?? "" }}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
