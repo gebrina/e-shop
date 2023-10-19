ecbfbbbabaf5b193492f133bc484c578466123ff
gebrina <gsonofmika@gmail.com>
2023-10-19 14:22:45 +0300

    added cart keys

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -11,14 +11,20 @@ export const DELETE_PRODUCT_KEY = "delete_product";
 export const UPDATE_PRODUCT_KEY = "updated_produt";
 export const GET_SINGLE_PRODUCT_KEY = "single_product";
 
-//User KEYS
+//USER KEYS
 export const GET_USER_KEY = "user";
 export const CREATE_USER_KEY = "create_user";
 export const DELETE_USER_KEY = "delete_user";
 export const UPDATE_USER_KEY = "update_user";
 
-//Order KEYS
+//ORDER KEYS
 export const GET_ORDER_KEY = "order";
 export const CREATE_ORDER_KEY = "create_order";
 export const DELETE_ORDER_KEY = "delete_order";
 export const UPDATE_ORDER_KEY = "update_order";
+
+//CART KEYS
+export const ADD_TO_CART_KEY = "add_to_cart";
+export const UPDATE_CART_KEY = "update_cart";
+export const GET_CART_KEY = "get_carts";
+export const DELETE_CART_KEY = "delete_cart";
