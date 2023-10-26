18e9558cebee7f305dcffda9769099ca57ee1af8
gebrina <gsonofmika@gmail.com>
2023-10-26 14:23:00 +0300

    updated payment complete page

--- src/payment/PaymentComplete.tsx
+++ src/payment/PaymentComplete.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useEcomContext } from "../context/EcomContext";
 import paymentcomplete from "../assets/pcomplete.png";
 import "./Payment.scss";
+import { FiArrowLeft, FiCheck, FiSkipBack } from "react-icons/fi";
+import { NavLink } from "react-router-dom";
 
 export const PaymentComplete = () => {
   const { handleResetCart } = useEcomContext();
@@ -9,5 +11,21 @@ export const PaymentComplete = () => {
     handleResetCart();
   }, [handleResetCart]);
 
-  return <section>Complete</section>;
+  return (
+    <section className="container center-items my-5">
+      <div className="payment-complete  bg-light">
+        <p className="fw-bold text-success success-icon">
+          <FiCheck />
+        </p>
+        <h1 className="text-success">Payment Successfull</h1>
+        <p>Thank you! Your payment is complete</p>
+        <NavLink
+          className={"btn btn-outline-success center-items fs-5 mb-3 mx-auto"}
+          to={"/products"}
+        >
+          <FiArrowLeft /> &nbsp;Products
+        </NavLink>
+      </div>
+    </section>
+  );
 };
