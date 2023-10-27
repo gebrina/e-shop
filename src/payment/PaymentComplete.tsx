0ebf3ed649a598255b4f86134a440e1a4ebbbbba
gebrina <gsonofmika@gmail.com>
2023-10-27 13:00:42 +0300

    fixed user shouldn't see the payment complete page with out payment

--- src/payment/PaymentComplete.tsx
+++ src/payment/PaymentComplete.tsx
@@ -2,25 +2,25 @@ import { useEffect, useRef } from "react";
 import { useEcomContext } from "../context/EcomContext";
 import "./Payment.scss";
 import { FiArrowLeft, FiCheck } from "react-icons/fi";
-import { NavLink } from "react-router-dom";
+import { NavLink, Navigate } from "react-router-dom";
 import axiosInstance from "../api/config";
 import { jwtDecode } from "../utils";
 
 export const PaymentComplete = () => {
-  const { handleResetCart } = useEcomContext();
+  const { handleResetCart, productsInCart } = useEcomContext();
   const decodedToken = jwtDecode();
 
   const paymentIntentRef = useRef(0);
 
   const paymentInterArray = location.href
-    .split("?")[1]
-    .split("&")[0]
-    .split("=");
+    ?.split("?")[1]
+    ?.split("&")[0]
+    ?.split("=");
 
   const handleCreatePayment = async (amount: number) => {
     try {
       if (paymentIntentRef.current < 1) {
-        const res = await axiosInstance.post("/payment", {
+        await axiosInstance.post("/payment", {
           amount,
           user: decodedToken?.user,
         });
@@ -30,20 +30,24 @@ export const PaymentComplete = () => {
   };
 
   useEffect(() => {
-    handleResetCart();
-    const retrievePaymentIntent = async () => {
-      try {
-        const response = await axiosInstance.get(
-          `/payment/retrieve/${paymentInterArray[1]}`
-        );
-        const { amount } = response.data;
-        handleCreatePayment(amount);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    retrievePaymentIntent();
+    if (productsInCart?.length) {
+      handleResetCart();
+      const retrievePaymentIntent = async () => {
+        try {
+          const response = await axiosInstance.get(
+            `/payment/retrieve/${paymentInterArray[1]}`
+          );
+          const { amount } = response.data;
+          handleCreatePayment(amount);
+        } catch (e) {
+          console.log(e);
+        }
+      };
+      retrievePaymentIntent();
+    }
   }, []);
+
+  if (!productsInCart?.length) return <Navigate to={"/products"} />;
 
   return (
     <section className="container center-items my-5">
