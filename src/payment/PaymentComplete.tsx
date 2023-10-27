b3a3d526db2449323ddb86366a170157be4f0360
gebrina <gsonofmika@gmail.com>
2023-10-27 12:52:32 +0300

    added save payment functionlaity

--- src/payment/PaymentComplete.tsx
+++ src/payment/PaymentComplete.tsx
@@ -1,14 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useEcomContext } from "../context/EcomContext";
 import "./Payment.scss";
 import { FiArrowLeft, FiCheck } from "react-icons/fi";
 import { NavLink } from "react-router-dom";
+import axiosInstance from "../api/config";
+import { jwtDecode } from "../utils";
 
 export const PaymentComplete = () => {
   const { handleResetCart } = useEcomContext();
+  const decodedToken = jwtDecode();
+
+  const paymentIntentRef = useRef(0);
+
+  const paymentInterArray = location.href
+    .split("?")[1]
+    .split("&")[0]
+    .split("=");
+
+  const handleCreatePayment = async (amount: number) => {
+    try {
+      if (paymentIntentRef.current < 1) {
+        const res = await axiosInstance.post("/payment", {
+          amount,
+          user: decodedToken?.user,
+        });
+      }
+      paymentIntentRef.current += 1;
+    } catch (e) {}
+  };
+
   useEffect(() => {
     handleResetCart();
-  }, [handleResetCart]);
+    const retrievePaymentIntent = async () => {
+      try {
+        const response = await axiosInstance.get(
+          `/payment/retrieve/${paymentInterArray[1]}`
+        );
+        const { amount } = response.data;
+        handleCreatePayment(amount);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    retrievePaymentIntent();
+  }, []);
 
   return (
     <section className="container center-items my-5">
--- src/payment/StripeWrapper.tsx
+++ src/payment/StripeWrapper.tsx
@@ -1,6 +1,6 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { Stripe, StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
-import axios from "axios";
+import axios from "../api/config";
 import { FC, useEffect, useState } from "react";
 import { PaymentForm } from "./PaymentForm";
 
@@ -18,9 +18,7 @@ export const StripeWrapper: FC<StripeWrapperProps> = ({ amount }) => {
   useEffect(() => {
     const getPublishableKey = async () => {
       try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_APP_API_URL}/payment/public-key`
-        );
+        const response = await axios.get(`/payment/public-key`);
         const stripe = loadStripe(response.data);
         setStripePromise(stripe);
       } catch (e) {}
